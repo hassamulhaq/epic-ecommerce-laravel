ba98f28a151b6bc754fd8e17c60738612785557d
Faizan Kamal <57044214+FaizanKamal7@users.noreply.github.com>
2023-10-05 23:12:08 +0500

    Removed extra spaces

--- database/migrations/2018_10_12_101803_create_country_translations_table.php
+++ database/migrations/2018_10_12_101803_create_country_translations_table.php
@@ -17,7 +17,6 @@ class CreateCountryTranslationsTable extends Migration
             $table->id();
             $table->string('locale');
             $table->text('name')->nullable();
-
             $table->foreignId('country_id')->constrained('countries')->cascadeOnDelete();
         });
     }
